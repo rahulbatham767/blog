fe68c653bdfc0d20b76b43a1521f8354e38bea00
rahulbatham767 <rahulbatham767@gmail.com>
2024-08-25 15:24:36 +0530

    storage

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { useAuth } from "../context/AuthContext";
+
 const Navbar = () => {
   const [authToken, setAuthToken] = useState(true);
   const router = useRouter();
