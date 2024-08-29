d379b2bf5bf801d2c47613bc82635ce920cdc73e
rahulbatham767 <rahulbatham767@gmail.com>
2024-08-30 00:40:37 +0530

    fix minor issue

--- app/page.js
+++ app/page.js
@@ -10,20 +10,17 @@ export default function Home() {
     fetchPosts(); // Fetch posts when the component mounts
   }, [fetchPosts]);
 
-  console.log(posts.length);
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Error: {isError}</p>;
-
+  if (isLoading)
+    return (
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <span className="loading loading-ring loading-lg "></span>
+      </div>
+    );
   return (
     <main className="min-h-screen min-w-screen">
       <div className="flex w-full justify-center mt-7">
         <p className="text-3xl">Blog Application</p>
       </div>
-      {isLoading && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <span className="loading loading-ring loading-lg "></span>
-        </div>
-      )}
       <div className="flex justify-center   mt-16">
         <div className="flex   items-center gap-5 flex-wrap">
           {posts.length > 0 ? (
