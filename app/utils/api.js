e215075485fb3a6d5ca4b48c631e720c3afea897
rahulbatham767 <rahulbatham767@gmail.com>
2024-09-06 18:40:34 +0530

    api set

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,23 +1,23 @@
 import axios from "axios";
 
 export const fetchPostsAPI = async () => {
-  const response = await axios.get("/post");
+  const response = await axios.get("/api/post");
   return response.data.post;
 };
 
 export const PostBlogAPI = async (blogPost) => {
-  const response = await axios.post("/post", blogPost);
+  const response = await axios.post("/api/post", blogPost);
   return response.data;
 };
 
 export const loginAPI = async (credentials) => {
-  const response = await axios.post("/user/login", credentials);
+  const response = await axios.post("/api/user/login", credentials);
 
   return response.data;
 };
 
 export const signupAPI = async (userData) => {
-  const response = await axios.post("/user/signup", userData);
+  const response = await axios.post("/api/user/signup", userData);
 
   return response.data;
 };
