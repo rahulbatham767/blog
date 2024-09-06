5fcf6500ef2a48639ce13359085a26f7f112dc47
rahulbatham767 <rahulbatham767@gmail.com>
2024-09-06 19:13:35 +0530

    db update

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,7 +1,16 @@
 import mongoose from "mongoose";
+
+let isConnected = false; // Track connection status
+
 const connectMongoDB = async () => {
   try {
+    if (isConnected) {
+      console.log("MongoDB connection is already established.");
+      return;
+    }
+
     await mongoose.connect(process.env.MONGODB_URI, { dbName: "blog" });
+    isConnected = true;
     console.log("connected to MongoDB");
   } catch (error) {
     console.log(error);
